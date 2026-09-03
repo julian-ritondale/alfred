079c25ce6de0cd8e3a8e05424f6fdf0f76a85fe3
Julian Ritondale <jritondale@mail.austral.edu.ar>
2026-09-02 22:58:17 -0300

    fix: add missing course config and change frontend origin url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express, { type Express } from 'express';
+import cors from 'cors';
 import taskRoutes from './modules/task/task.routes.ts';
 import { errorHandler } from './middleware/error.middleware.ts';
 
 const app: Express = express();
+
+const allowedOrigins = process.env.CORS_ORIGINS?.split(',') ?? [];
+
+app.use(cors({
+  origin: allowedOrigins,
+}));
 
 app.use(express.json());
 
